413f22aa8e683c3b3ea0fa63f647e04b36179d24
amitsason3 <amitsason3@gmail.com>
2024-10-17 21:46:17 +0300

    added contact.js to routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const homeRoutes = require('./routes/home')
 const adminPanelRoutes = require('./routes/admin_panel')
 const itemRoutes = require('./routes/item')
 const confirmRouts = require('./routes/confirm')
+const contactRoutes = require('./routes/contact');
 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -29,5 +30,6 @@ server.use('/',homeRoutes)
 server.use('/admin',adminPanelRoutes)
 server.use('/item',itemRoutes)
 server.use('/confirm', confirmRouts)
+app.use('/contact', contactRoutes);
 
 server.listen(80)
